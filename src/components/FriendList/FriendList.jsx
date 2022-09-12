98411e5b8fcd6964e48c8538a577d2a656c30385
Sarat862 <sarat862@gmail.com>
2022-09-13 00:39:00 +0300

    Add component FriendListItem

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,17 +1,20 @@
 import PropTypes from 'prop-types';
 import css from './FriendList.module.css';
 
+import { FriendListItem } from './FriendListItem'
+
 export function FriendList({friends}) {
-    const elements = friends.map(({ id, avatar, name, isOnline }) => 
-        <li className={css.item} key={id}>
-            <span className={isOnline ? css.online : css.offline}></span>
-            <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
-            <p className={css.name}>{name}</p>
-        </li>)
 
     return (
         <ul className={css.friendList}>
-            {elements}
+            {friends.map(({ id, avatar, name, isOnline }) =>
+                <FriendListItem
+                    id={id}
+                    avatar={avatar}
+                    name={name}
+                    isOnline={isOnline}
+                />
+            )}
         </ul>
     )
 }
